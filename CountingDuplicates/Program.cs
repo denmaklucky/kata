e042a679753b92c70fa7208655cbbb0f943f1da4
Denis Makarenko <denis-makarenko@outlook.com>
2022-02-13 15:53:28 +0200

    Add a new kata `DuplicateCount`

---
+++ CountingDuplicates/Program.cs
@@ -0,0 +1,7 @@
+﻿static int DuplicateCount(string str)
+{
+    return str.ToLowerInvariant().GroupBy(g => g).Count(g => g.Count() > 1);
+}
+
+var temp = DuplicateCount("AAAb");
+Console.WriteLine(temp);
