78402513f64765c7c129c0a67ff0dbcf3646ae76
Denis Makarenko <denis-makarenko@outlook.com>
2022-08-07 22:19:34 +0300

    First solution

---
+++ CountIdAddress/IpAddressDifference.cs
@@ -0,0 +1,30 @@
+﻿namespace CountIdAddress;
+
+public class IpAddressDifference
+{
+    
+    private readonly byte _start;
+    private readonly byte _end;
+    private readonly byte _weight;
+
+    public IpAddressDifference(byte start, byte end, byte weight)
+    {
+        _start = start;
+        _end = end;
+        _weight = weight;
+    }
+
+    public int GetDifference()
+    {
+        var difference = _end - _start;
+        return difference == 0 ? _weight : difference;
+    }
+}
+
+public enum MaskPosition
+{
+    One,
+    Two,
+    Three,
+    Four
+}
--- CountIdAddress/Program.cs
+++ CountIdAddress/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static long IpsBetween(string start, string end)
+{
+    const int ipAddressLength = 4;
+
+    var startBytes = start.Split('.');
+    var endBytes = end.Split('.');
+
+    var totalRemainder = 0;
+    var currentRamainder = 0;
+    var totalDifference = 0;
+
+    for (var i = ipAddressLength - 1; i >= 0; i--)
+    {
+        var difference = byte.Parse(endBytes[i]) - byte.Parse(startBytes[i]);
+
+        if (i != 0 && difference == 0)
+        {
+            totalRemainder += 1;
+        }
+
+        totalDifference += difference;
+
+        if (i == 0 && totalDifference == 0)
+        {
+            totalRemainder = 0;
+        }
+    }
+
+    Console.WriteLine($"Total difference: {totalDifference}, remainder: {totalRemainder}");
+
+    return totalDifference + totalRemainder * 256;
+}
+
+
+Console.WriteLine(IpsBetween("10.0.0.0", "10.0.0.50"));
