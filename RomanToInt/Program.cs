5eced6fd74a15aa96466cbb0b295ac5802ffad8b
Denis Makarenko <denis-makarenko@outlook.com>
2022-08-11 08:27:44 +0300

    Some changes

--- RomanToInt/Program.cs
+++ RomanToInt/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int RomanToInt(string s)
+{
+    var romanNumerals = new Dictionary<char, int>
+    {
+        { 'I', 1 },
+        { 'V', 5 },
+        { 'X', 10 },
+        { 'L', 50 },
+        { 'C', 100 },
+        { 'D', 500 },
+        { 'M', 1000 }
+    };
+
+    var arabicNumerals = s.Select(d => romanNumerals[d]).ToArray();
+
+    var targetInt = 0;
+    for (var i = arabicNumerals.Length - 1; i >= 0; i--)
+    {
+        if (i == arabicNumerals)
+        {
+        }
+    }
+}
